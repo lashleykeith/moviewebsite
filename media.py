ea0eace77c0bb49ce98874a36418d670102d712c
Keith Lashley <lashleykeith@gmail.com>
2017-11-01 23:45:03 +0900

    Add files via upload

--- media.py
+++ media.py
@@ -1,7 +1,6 @@
 import webbrowser
 
 class Movie():
-	""" This class provides a way to store movie related information """
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 	def __init__(self, mymovietitle, mymoviestoryline, myposterimage, mytraileryoutube):
 		self.title = mymovietitle
